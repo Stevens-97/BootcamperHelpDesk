77c2b92fd43a985d08b8c954946873655eb2fcb0
Elliott <Elliott.Samuel.Stevens@gmail.com>
2023-11-16 22:23:00 +0000

    Add project files.

---
+++ BootcamperHelpDesk/Models/Feedback.cs
@@ -0,0 +1,12 @@
+﻿namespace BootcamperHelpDesk.Models
+{
+    public class Feedback
+    {
+        public int ID { get; set; }
+        public string Response { get; set; }
+        public int UserId { get; set; }
+        public int QuestionID { get; set; }
+        public int SurveryId { get; set; }
+        public DateOnly CreationDate { get; set; }
+    }
+}
