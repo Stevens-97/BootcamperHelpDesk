4bbce74d4813183b40f4982fbb59a6c3d4129922
Elliott <Elliott.Samuel.Stevens@gmail.com>
2023-11-18 19:17:29 +0000

    Create service response model

---
+++ BootcamperHelpDesk/Models/ServiceResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace bootcamper_helpdesk.Models
+{
+    public class ServiceResponse<T>
+    {
+        public T? Data { get; set; }
+        public bool? Success { get; set; } = true;
+        public string Message { get; set; } = string.Empty;
+    }
+}
