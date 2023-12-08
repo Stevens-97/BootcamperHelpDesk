5ef4e75365a3943122b318b4adffc491218b4fb7
Elliott <Elliott.Samuel.Stevens@gmail.com>
2023-12-08 04:31:09 +0000

    Update controllers

--- BootcamperHelpDesk/Controllers/ResponseController.cs
+++ BootcamperHelpDesk/Controllers/ResponseController.cs
@@ -1,5 +1,4 @@
-﻿using bootcamper_helpdesk.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace bootcamper_helpdesk.Controllers
 {
@@ -18,20 +17,35 @@ namespace bootcamper_helpdesk.Controllers
         [HttpGet("getSurveyResponses/{id}")]
         public async Task<ActionResult<ServiceResponse<List<GetResponsesResponseDto>>>> GetResponses(int id)
         {
-            return Ok(await _responseService.GetResponses(id));
+            var response = await _responseService.GetResponses(id);
+
+            if (response == null)
+            {
+                return NotFound(response);
+            }
+            return Ok(response);
         }
 
         [HttpGet("GetSingleResponse")]
         public async Task<ActionResult<ServiceResponse<List<GetResponsesResponseDto>>>> GetSingleResponse(int id, int questionId)
         {
-            return Ok(await _responseService.GetSingleResponse(id, questionId));
+            var response = await _responseService.GetSingleResponse(id, questionId);
+            if (response == null)
+            {
+                return NotFound(response);
+            }
+            return Ok(response);
         }
 
         [HttpPost("PostSurveyResponses")]
         public async Task<ActionResult<ServiceResponse<List<GetResponsesResponseDto>>>> AddResponses(List<AddResponsesRequestDto> newResponses)
         {
-
-            return Ok(await _responseService.AddResponses(newResponses));
+            var response = _responseService.AddResponses(newResponses);
+            if(response == null)
+            {
+                return NotFound(response);
+            }
+            return Ok(response);
         }
     }
 }
--- BootcamperHelpDesk/Controllers/UserController.cs
+++ BootcamperHelpDesk/Controllers/UserController.cs
@@ -1,17 +1,71 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using bootcamper_helpdesk.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace bootcamper_helpdesk.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class UserController : ControllerBase
     {
-        // Set the http request
+        
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
 
         // Set the API route
-        [HttpGet("api/[controller]")]
+        [HttpGet("{id}")]
         // Make a function, that is async task then uses Dto as types
-        public Task<> GetUser()
+        public async Task<ActionResult<ServiceResponse<GetUserResponseDto>>> GetUser(int id)
         {
-            return Ok();
+            var response = await _userService.GetUser(id);
+
+            if (response == null)
+            {
+                return NotFound(response);
+
+            }
+            return Ok(response);
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ServiceResponse<GetUserResponseDto>>> AddUser(AddUserRequestDto newUser)
+        {
+            var response = await _userService.AddUser(newUser);
+            if (response == null)
+            {
+                return NotFound(response);
+            }
+            return Ok(response);
+
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<ServiceResponse<GetUserResponseDto>>> DeleteUser(int id)
+        {
+            var response = await _userService.DeleteUser(id);
+            if (response.Data == null)
+            {
+                return NotFound(response);
+            }
+
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<ServiceResponse<GetUserResponseDto>>> UpdateUser(UpdateUserRequestDto updatedUser)
+        {
+            var response = await _userService.UpdateUser(updatedUser);
+
+            if (response.Data == null)
+            {
+                return NotFound(response);
+            }
+
+            return Ok(response);
         }
     }
 }
