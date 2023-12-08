bb7c0eb3bfb32a248cfaaca00474c9fdfbec6b1e
Elliott <Elliott.Samuel.Stevens@gmail.com>
2023-12-08 04:31:23 +0000

    Update Dto

--- BootcamperHelpDesk/Dtos/User/UpdateUserRequestDto.cs
+++ BootcamperHelpDesk/Dtos/User/UpdateUserRequestDto.cs
@@ -2,11 +2,12 @@
 {
     public class UpdateUserRequestDto
     {
+        public int Id { get; set; }
+
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public string? Email { get; set; }
         public string? Password { get; set; }
         public int AutherisationLevel { get; set; }
-        public DateTimeOffset CreatedAt { get; set; }
     }
 }
